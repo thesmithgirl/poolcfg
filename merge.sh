510b268aeacf0199e95eecad0a939c2e79a73c48
thesmithgirl <4592591+thesmithgirl@users.noreply.github.com>
2021-01-20 20:26:49 +0100

    Add magiline for merging to index.html

--- merge.sh
+++ merge.sh
@@ -7,6 +7,14 @@ cat \
    Magiline/M00_header.html \
    Magiline/M01_poolsize.html \
    Magiline/M02_cleaner.html \
+   Magiline/M03_stairs.html \
+   Magiline/M04_poolfoil.html \
+   Magiline/M05_spotlight.html \
+   Magiline/M06_heating.html \
+   Magiline/M07_cover.html \
+   Magiline/M08_dosing.html \
+   Magiline/M09_accessories.html \
    Magiline/M99_footer.html \
+   98_result.html \
    99_footer.html \
    > index.html
