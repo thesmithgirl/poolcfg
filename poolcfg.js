94016b627e21656d5f7d163f8dceb00a0ac3cbda
thesmithgirl <4592591+thesmithgirl@users.noreply.github.com>
2021-01-29 03:38:22 +0100

    Add functions for getting selected Laghetto Buttons

--- poolcfg.js
+++ poolcfg.js
@@ -1,15 +1,43 @@
 function cfg_summary_l () {
   var l_cfg = "";
   l_cfg += get_lheating();
+  l_cfg += "\n";
+  l_cfg += get_lpoolsize();
+  l_cfg += "\n";
+  l_cfg += get_lcover();
+  l_cfg += "\n";
+  l_cfg += get_ldosing();
+
 
 
 $("#l_cfg").text(l_cfg);
+}
+
+function get_lpoolsize() {
+  var l_poolsize = $("input[name='l_poolsize']:checked").parent('label').text();
+
+  return l_poolsize;
+
 }
 
 function get_lheating() {
   var l_heating = $("input[name='l_heating']:checked").parent('label').text();
 
   return l_heating;
+
+}
+
+function get_lcover() {
+  var l_cover = $("input[name='l_cover']:checked").parent('label').text();
+
+  return l_cover;
+
+}
+
+function get_ldosing() {
+  var l_dosing = $("input[name='l_dosing']:checked").parent('label').text();
+
+  return l_dosing;
 
 }
 
